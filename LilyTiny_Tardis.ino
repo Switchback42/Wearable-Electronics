e39e34799ad583d10773d01a1b17a2400cfd8ae4
Switchback42 <40530437+Switchback42@users.noreply.github.com>
2018-11-17 18:36:11 -0500

    Create LilyTiny_Tardis.ino

---
+++ LilyTiny_Tardis.ino
@@ -0,0 +1,159 @@
+//Saved as LilyTiny_Tardis.ino
+//This code works on an Attiny85 connected to 4 NeoPixels on pin 0 and one LilyPad RBG LED on pins 1-3
+//It may be powered through a couple of coin cell batteries.
+
+Created by Jill Dawson (relying heavily on Adafruit sample code).
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 0
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, PIN, NEO_GRB + NEO_KHZ800);
+
+#define PIXEL_PIN 0
+#define PIXEL_COUNT 4
+#define PIXEL_TYPE WS2812B 
+
+//Place a LilyPad RGB LED (common anode) on top of the Tardis
+
+int redPin = 3;
+int greenPin = 1;
+int bluePin = 2;
+
+//uncomment this line if using a Common Anode (positive) LED
+#define COMMON_ANODE
+ 
+void setup() {
+ strip.begin();
+ 
+ pinMode(redPin, OUTPUT);
+ pinMode(greenPin, OUTPUT);
+ pinMode(bluePin, OUTPUT); 
+ 
+ digitalWrite(redPin,LOW); //turn off LED on top
+ digitalWrite(greenPin,LOW);
+ digitalWrite(bluePin, LOW);
+ 
+ strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop()
+{
+{ //Top of Tardis
+  setColor(0, 255, 255);  // aqua
+  delay(1000);
+  setColor(255, 0, 0);  // red
+  delay(1000);
+  setColor(0, 255, 0);  // green
+  delay(1000);
+  setColor(0, 0, 255);  // blue
+  delay(1000);
+  setColor(255, 255, 0);  // yellow
+  delay(1000);  
+  setColor(80, 0, 80);  // purple
+  delay(1000);
+  setColor(0, 255, 255);  // aqua
+  delay(1000);
+}
+
+// Send a color wip in...  
+colorWipe(strip.Color(127, 127, 127), 50); // White
+colorWipe(strip.Color(0, 0, 255), 50); // Blue
+
+// Send a theater pixel chase in...
+theaterChase(strip.Color(127, 127, 127), 50); // White
+theaterChase(strip.Color(127, 0, 0), 50); // Red
+theaterChase(strip.Color(0, 0, 127), 50); // Blue
+
+rainbow(20);
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) { //you need the wheel procedure below for this to work
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+if(WheelPos < 85) {
+return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+} else if(WheelPos < 170) {
+WheelPos -= 85;
+return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+} else {
+WheelPos -= 170;
+return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+   }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+void setColor(int red, int green, int blue) //Tardis Top
+{
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
+}
+
